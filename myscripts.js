8e59a4db8bf7bbecafa85861985df319363b3661
Anthony Stender <anthony.m.stender@gmail.com>
2019-02-25 06:57:54 -0600

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -8,3 +8,29 @@ function myFunction() {
 function alertFunction() {
 	alert("Alert!");
 }
+
+function dueDate() {
+	
+	var dayMS = new Date().getTime() + (86400000 * 4);
+	var ifSaturday = new Date().getTime() + (86400000 * 3);
+	var ifSunday = new Date().getTime() + (86400000 * 2);
+	var taskDate = new Date(dayMS);
+	
+	var dayOfWeek = taskDate.getDay();
+	if(dayOfWeek == 0) {
+		var taskDate = new Date(ifSunday)
+	} else if (dayOfWeek == 6) {
+		var taskDate = new Date(ifSaturday)
+	} else {
+		taskDate = taskDate;
+	}
+
+	var month = taskDate.getMonth()+1;
+	var day = taskDate.getDate();
+	var year = taskDate.getFullYear();
+	var newdate = month + "/" + day + "/" + year;
+	
+	document.getElementById("demo").innerHTML = "The due date is: " + newdate;
+	
+	
+}
